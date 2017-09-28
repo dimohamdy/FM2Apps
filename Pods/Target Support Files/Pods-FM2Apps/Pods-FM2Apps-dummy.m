294689bc3177e7bda6e8b2b9c2988bc158c877da
dimohamdy <dimo.hamdy@gmail.com>
2017-09-28 15:06:27 +0200

    remove pods folder

--- Pods/Target Support Files/Pods-FM2Apps/Pods-FM2Apps-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FM2Apps : NSObject
-@end
-@implementation PodsDummy_Pods_FM2Apps
-@end
--- Pods/Target Support Files/Pods-FM2AppsUITests/Pods-FM2AppsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FM2AppsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FM2AppsUITests
-@end
